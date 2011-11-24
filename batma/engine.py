a8bb2fb90ec4648df313e6e61f249d0f46c4bf46
Renato Pereira <renato.ppontes@gmail.com>
2011-11-24 21:28:21 -0200

    schedule functions in batma

--- batma/engine.py
+++ batma/engine.py
@@ -236,7 +236,9 @@ class Game(WindowProxy):
         self.camera = Camera(self.center)
         self.auto_clear = True
         self.show_fps = False
-
+        self.scheduled_calls = []
+        self.scheduled_interval_calls = []
+        
         # Input
         self.keyboard = KeyboardState()
         self.mouse = MouseState()
@@ -258,6 +260,110 @@ class Game(WindowProxy):
         pyglet.resource.reindex()
         self.load_content()
         
+
+    def schedule_interval(self, callback, interval, *args, **kwargs):
+        """
+        Schedule a function to be called every `interval` seconds.
+
+        Specifying an interval of 0 prevents the function from being
+        called again (see `schedule` to call a function as often as possible).
+
+        The callback function prototype is the same as for `schedule`.
+
+        :Parameters:
+            `callback` : function
+                The function to call when the timer lapses.
+            `interval` : float
+                The number of seconds to wait between each call.
+
+        This function is a wrapper to pyglet.clock.schedule_interval.
+        It has the additional benefit that all calllbacks are paused and
+        resumed when the node leaves or enters a scene.
+
+        You should not have to schedule things using pyglet by yourself.
+        """
+
+        pyglet.clock.schedule_interval(callback, interval, *args, **kwargs)
+        self.scheduled_interval_calls.append(
+            (callback, interval, args, kwargs)
+        )
+
+    def schedule(self, callback, *args, **kwargs):
+        """
+        Schedule a function to be called every frame.
+
+        The function should have a prototype that includes ``dt`` as the
+        first argument, which gives the elapsed time, in seconds, since the
+        last clock tick.  Any additional arguments given to this function
+        are passed on to the callback::
+
+            def callback(dt, *args, **kwargs):
+                pass
+
+        :Parameters:
+            `callback` : function
+                The function to call each frame.
+
+        This function is a wrapper to pyglet.clock.schedule.
+        It has the additional benefit that all calllbacks are paused and
+        resumed when the node leaves or enters a scene.
+
+        You should not have to schedule things using pyglet by yourself.
+        """
+        pyglet.clock.schedule(callback, *args, **kwargs)
+        self.scheduled_calls.append((callback, args, kwargs))
+
+    def unschedule(self, callback):
+        """
+        Remove a function from the schedule.
+
+        If the function appears in the schedule more than once, all occurances
+        are removed.  If the function was not scheduled, no error is raised.
+
+        :Parameters:
+            `callback` : function
+                The function to remove from the schedule.
+
+        This function is a wrapper to pyglet.clock.unschedule.
+        It has the additional benefit that all calllbacks are paused and
+        resumed when the node leaves or enters a scene.
+
+        You should not unschedule things using pyglet that where scheduled
+        by node.schedule/node.schedule_interface.
+        """
+        self.scheduled_calls = [
+            c for c in self.scheduled_calls if c[0] != callback
+        ]
+        self.scheduled_interval_calls = [
+            c for c in self.scheduled_interval_calls if c[0] != callback
+        ]
+
+        pyglet.clock.unschedule(callback)
+
+    def resume_scheduler(self):
+        """
+        Time will continue/start passing for this node and callbacks
+        will be called, worker actions will be called
+        """
+        for c, i, a, k in self.scheduled_interval_calls:
+            pyglet.clock.schedule_interval(c, i, *a, **k)
+        for c, a, k in self.scheduled_calls:
+            pyglet.clock.schedule(c, *a, **k)
+
+    def pause_scheduler(self):
+        """
+        Time will stop passing for this node: scheduled callbacks will
+        not be called, worker actions will not be called
+        """
+        for f in set(
+            [x[0] for x in self.scheduled_interval_calls] +
+            [x[0] for x in self.scheduled_calls]
+                ):
+            pyglet.clock.unschedule(f)
+        for arg in self.scheduled_calls:
+            pyglet.clock.unschedule(arg[0])
+
+
     def on_update(self, tick):
         for scene in self._scenes:
             scene.update(tick)
@@ -272,7 +378,7 @@ class Game(WindowProxy):
         '''
         if self.auto_clear:
             self.clear()
-            
+
         pyglet.gl.glPushMatrix()
         self.camera.reset(self.center)
 
